1c02a952f23eb9f8b49f80ab6540fce0815173a9
CharlyesRodrigues <charlyessouza@gmail.com>
2022-10-08 17:53:35 -0300

    Update Operadores.js

--- Operadores.js
+++ Operadores.js
@@ -1,2 +1,41 @@
-const a = 5;
+//                                         Desafio | Operadores
+//                                              Aritméticos
+// A - Faça uma potência que retorne o número 9;
+
+let  a = 3 ;
+let b = 2 ;
+console.log("Exemplo de potência");
+console.log("A potência de 3 é :" + a ** b);
+// Resultado no terminal é 9
+
+//B - Crie uma variável que inicie com o valor 1, e faça ela chegar até o número 7 usando o operador incremento;
+
+console.log("Exemplo de incremento ++");
+var inicio = 1;
+console.log(inicio++);
+console.log(inicio++);
+console.log(inicio++);
+console.log(inicio++);
+console.log(inicio++);
+console.log(inicio++);
+console.log(inicio++); 
+// Resultado será = 1,2,3,4,5,6,7 . 
+
+//                                                Atribuição
+//A - Crie uma variável que comece com o valor 10 e que receba uma soma (+=), onde o resultado deve ser maior do que 20;
+
+console.log("Exemplo de atribuição com incremento +=");
+var valor_inicio = 10;
+
+ valor_inicio += 11;
+
+console.log(valor_inicio);
+
+//                                                  Comparação
+// A - Compare uma string com o valor “1” a um número com o valor 1,o resultado deve retornar false;
+
+
+
+//                                                   Lógicos
+// A - Use o operador OR (ou) para retornar true*/
 
