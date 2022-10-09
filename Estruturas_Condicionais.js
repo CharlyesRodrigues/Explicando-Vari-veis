09e5d457ee0e965a30f9574f872e2e9a39eaa1e5
CharlyesRodrigues <charlyessouza@gmail.com>
2022-10-09 19:59:00 -0300

    Update Estruturas_Condicionais.js

--- Estruturas_Condicionais.js
+++ Estruturas_Condicionais.js
@@ -34,6 +34,9 @@ if(saque + divida >= valor_total || bloqueada ){
 }
 console.log("==========================");
 // Exemplo de estrutura alinhada
+// OBSERVAÇÃO : CUIDADO AO USAR O MÓDULO DE ERRO DESSE EXEMPLO, POIS ELE ACABA COM O PROCESSO DO SEU SISTEMA.
+// UMA FORMA DE BOA UTILIZAÇÃO SERIA EM UM ATENDIMENTO AONDE A PESSOA DIGITOU UMA OPÇÃO ERRADA E DEVIDO A ISSO
+// O ATENDIMENTO PODE SER ENCERRADO
 console.log("Exemplo de estrutura alinhada");
 
 const  opcao = 3;
@@ -49,6 +52,47 @@ if(opcao === 1){
 
 }else{
 
-   throw new Error("Operação inválida");
+    throw new Error("Operação inválida"); // módulo de erro do JavaScript
 
 }
+
+
+// Exemplo de Switch case
+console.log("==========================");
+console.log("Exemplo de Switch case");
+
+const username = "coordenador";
+
+
+switch(username){
+case "admin":
+        console.log("Bem vindo administrador")
+      break;
+case "coordenador":
+        console.log("Bem vindo coordenador")
+      break;
+default:
+        console.log("Bem vindo")
+    
+}
+
+// Exemplo de Operador ternário
+console.log("==========================");
+console.log("Exemplo de Operador ternário");
+
+const idade = 10;
+
+idade > 18 ? console.log("Bem vindo") : console.log("Precisa de autorização"); // Uma forma de se fazer
+console.log(idade > 18 ? "Seja bem vindo" : "Precisa de autorização dos pais"); // Essa segunda forma é bem melhor
+
+
+// Exemplo de Diminuindo o IF/Else
+console.log("==========================");
+console.log("Exemplo de Diminuindo o IF/Else");
+
+function validaIdade(idade){
+if (idade >= 18) return "Bem Vindo"
+return "Opção Inválida"
+
+}
+console.log(validaIdade(17));
