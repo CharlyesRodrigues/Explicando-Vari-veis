3b329ca76d64ae15223614aef31314de88769426
CharlyesRodrigues <charlyessouza@gmail.com>
2022-10-09 20:18:18 -0300

    Update Praticando_Estruturas_Condicionais.js

--- Praticando_Estruturas_Condicionais.js
+++ Praticando_Estruturas_Condicionais.js
@@ -1,1 +1,22 @@
-asdsadsa
+function calculadora(numero1,numero2,operacao){
+
+    if(operacao === 1 ){
+    return (numero1 + numero2);
+
+    }else if(operacao === 2 ){
+        return (numero1 - numero2);
+    
+    }else if(operacao === 3  ){
+            return (numero1 / numero2 );
+        
+     }else if(operacao === 4  ){
+        return (numero1 * numero2 );
+     }else {
+
+        return "OPÇÃO INVÁLIDA"
+     }
+
+}
+
+
+console.log(calculadora(4,2,5));
