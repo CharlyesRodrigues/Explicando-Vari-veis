7f52c6b7528c23d650fcf125c6985f75a6952d9a
CharlyesRodrigues <charlyessouza@gmail.com>
2022-10-09 16:34:34 -0300

    Update Praticando_funcoes.js

--- Praticando_funcoes.js
+++ Praticando_funcoes.js
@@ -33,9 +33,51 @@ B - Crie uma função que receba a seguinte lista como parâmetro:
 1 [“Júlia”, “Anna”, “Pedro”, “Rafael”, “Paula”]
 Essa função deve retornar a mesma lista porém com um filter que só
 retorne os nomes que começam com a letra “P” */
+/**
+ * 
+ * @param {list} x 
+ * @description Função que recebe uma lista como parâmetro e retorna um filter
+ * aonde só possui nomes que começam com uma determinada letra  que nesse caso é a "P"
+ */
+
+function listas(x){
+
+console.log("Os nomes selecionados pelo filter são: " + x.filter(nomes => nomes[0] == "P") );
+
+}
+
+const array_listas = ["Julia","Ana","Pedro","Rafael","Paula"];
+listas(array_listas);
 
 
-const nomes_lista = (array_lista = ["Julia","Ana","Pedro","Rafael","Paula"]) => array_lista.filter(nomes => nomes[0] == "P") ;
+/* 4º Desafio
+3 - Função e Operadores
+A - Crie uma função que receba dois números e retorne a soma dos mesmos.*/
 
+function somatoria(n5,n8){
 
-console.log(nomes_lista);
+return (n5 + n8);
+
+}
+
+console.log("A soma dos dois números é : " + somatoria(15,5));
+
+//B - Crie uma função que recebe dois números e retorne a multiplicação entre eles
+
+function multiplicacao(n6,n7){
+
+console.log("A multiplicação dos dois números é : "+ n6*n7);
+    
+    }
+    
+ multiplicacao(5,5);
+
+// C - Crie uma função que receba dois números e retorne o média entre eles
+
+function media(n6,n7){
+
+    console.log("A média dos dois números é : " + (n6+n7)/2);
+        
+        }
+        
+     media(2,5);   
